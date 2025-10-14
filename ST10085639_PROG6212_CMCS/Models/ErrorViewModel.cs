2105657886a1d9f79a330d25c493f861c2922dfa
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-14 21:19:54 +0200

    Add project files.

---
+++ ST10085639_PROG6212_CMCS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10085639_PROG6212_CMCS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
