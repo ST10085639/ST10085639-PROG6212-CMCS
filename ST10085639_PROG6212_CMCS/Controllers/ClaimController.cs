4cd532cd3d5177d4b4823430cdfc07ec2e34c75b
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-15 20:11:50 +0200

    Initial ClaimController

---
+++ ST10085639_PROG6212_CMCS/Controllers/ClaimController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ST10085639_PROG6212_CMCS.Controllers
+{
+    public class ClaimController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
