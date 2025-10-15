7d6b3b6b1e8bfddee82a3b5800b28ab18171828c
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-15 15:27:02 +0200

    AuthenticationController basic methods added

--- ST10085639_PROG6212_CMCS/Controllers/AuthenticationController.cs
+++ ST10085639_PROG6212_CMCS/Controllers/AuthenticationController.cs
@@ -1,12 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ST10085639_PROG6212_CMCS.Data;
 
 namespace ST10085639_PROG6212_CMCS.Controllers
 {
     public class AuthenticationController : Controller
     {
-        public IActionResult Index()
+        public readonly ApplicationDbContext _db;
+
+        public AuthenticationController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp() => View();
+
+        [HttpPost]
+        public IActionResult SignUp(User model)
+        {
+            
+        }
+
+        [HttpGet]
+        public IActionResult Login() => View();
+
+        [HttpPost]
+        public IActionResult Login(string email, string password)
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Login", "Authentication");
         }
     }
 }
