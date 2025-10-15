09fc59af5acd1bb7cfc755764cb36f5b8c75aa4c
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-15 15:45:47 +0200

    AuthenticationController Complete

--- ST10085639_PROG6212_CMCS/Controllers/AuthenticationController.cs
+++ ST10085639_PROG6212_CMCS/Controllers/AuthenticationController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿//References:
+
+using Microsoft.AspNetCore.Mvc;
 using ST10085639_PROG6212_CMCS.Data;
+using ST10085639_PROG6212_CMCS.Models;
 
 namespace ST10085639_PROG6212_CMCS.Controllers
 {
@@ -18,7 +21,12 @@ namespace ST10085639_PROG6212_CMCS.Controllers
         [HttpPost]
         public IActionResult SignUp(User model)
         {
-            
+            if (!ModelState.IsValid) return View(model);
+
+            _db.Users.Add(model);
+            _db.SaveChanges();
+
+            return RedirectToAction("Login");
         }
 
         [HttpGet]
@@ -27,7 +35,27 @@ namespace ST10085639_PROG6212_CMCS.Controllers
         [HttpPost]
         public IActionResult Login(string email, string password)
         {
+            var user = _db.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+            if (user != null)
+            {
+                HttpContext.Session.SetString("UserId", user.ID.ToString());
+                HttpContext.Session.SetString("Role", user.Role ?? "");
 
+                //Role-based redirection
+                switch (user.Role)
+                {
+                    case "Lecturer":
+                        return RedirectToAction("Create", "Claim"); //Submit Claims
+                    case "Programme Coordintor":
+                    case "Academic Manager":
+                        return RedirectToAction("AdminView", "Claim"); //Admin Page
+                    default:
+                        return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return View();
         }
 
         [HttpPost]
