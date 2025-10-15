4f2bff18664841c7ff38a8d2c51cda29e87b160d
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-15 20:30:07 +0200

    ClaimController basic methods added

--- ST10085639_PROG6212_CMCS/Controllers/ClaimController.cs
+++ ST10085639_PROG6212_CMCS/Controllers/ClaimController.cs
@@ -1,12 +1,80 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using ST10085639_PROG6212_CMCS.Data;
+using ST10085639_PROG6212_CMCS.Models;
 
 namespace ST10085639_PROG6212_CMCS.Controllers
 {
     public class ClaimController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public ClaimController(ApplicationDbContext db)
         {
+            _db = db;
+        }
+
+        public IActionResult AdminView()
+        {
+            if (!IsAdmin())
+                return Unauthorized();
+
+            var claims = _db.Claims.ToList();
+            return View(claims);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            if (!IsLecturer())
+                return Unauthorized();
+
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Claim model, IFormFile document)
+        {
+            if (!IsLecturer()) return Unauthorized();
+
+            _db.Claims.Add(model);
+            _db.SaveChanges();
+
+            return RedirectToAction("LecturerHistory");
+        }
+
+        public IActionResult LecturerHistory()
+        {
+
+        }
+
+        public IActionResult DownloadDocument(string documentPath)
+        {
+
+        }
+
+        private bool IsAdmin()
+        {
+            var role = HttpContext.Session.GetString("Role");
+            return role == "Programme Coodinator" || role == "Academic Manager";
+        }
+
+        private bool IsLecturer()
+        {
+            var role = HttpContext.Session.GetString("Role");
+            return role == "Lecturer";
+        }
+
+        [HttpPost]
+        public IActionResult Approve(int claimID)
+        {
+
+        }
+
+        [HttpPost]
+        public IActionResult Reject(int claimID)
+        {
+            
         }
     }
 }
