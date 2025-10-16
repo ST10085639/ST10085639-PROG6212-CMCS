7cb25b2484d0dd47d35615cd8b487459036768b7
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-16 13:35:32 +0200

    ClaimTests complete without references. Will add from notepad at the end.

--- ST10085639_PROG6212_CMCS.Tests/ClaimTests.cs
+++ ST10085639_PROG6212_CMCS.Tests/ClaimTests.cs
@@ -12,6 +12,18 @@ namespace ST10085639_PROG6212_CMCS.Tests.Models
         [TestMethod]
         public void CalculateTotal_ShouldReturnCorrectAmount()
         {
+            //Arrange
+            var claim = new Claim
+            {
+                HoursWorked = 10,
+                HourlyRate = 150
+            };
+
+            //Action
+            var result = claim.CalculateTotal();
+
+            //Assert
+            Assert.AreEqual(1500, result);
         }
     }
 }
