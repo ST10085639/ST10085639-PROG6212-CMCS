7ca788bb37841c35f407525e2c42c3c3193e2acc
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-16 13:18:22 +0200

    Initializing Unit Testing Project

---
+++ ST10085639_PROG6212_CMCS.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace ST10085639_PROG6212_CMCS.Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
