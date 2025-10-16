5637085c11f0f515af2af4d68dff75747e9d2027
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-16 20:42:31 +0200

    Added comments and references to the Program class

--- ST10085639_PROG6212_CMCS/Program.cs
+++ ST10085639_PROG6212_CMCS/Program.cs
@@ -1,11 +1,16 @@
+// Microsoft Learn. 2024. ASP.NET Core application startup, 12 December 2024. [Online]. Available at: https://learn.microsoft.com/en-us/aspnet/core/fundamentals/startup [Accessed 16 October 2025].
+// Microsoft Learn. 2024. Routing in ASP.NET Core, 18 September 2024. [Online]. Available at:https://learn.microsoft.com/en-us/aspnet/core/fundamentals/routing [Accessed 16 October 2025].
+// Microsoft Learn. 2024. Session state in ASP.NET Core, 24 April 2024. [Online]. Available at:https://learn.microsoft.com/en-us/aspnet/core/fundamentals/app-state [Accessed 16 October 2025].
+
 using ST10085639_PROG6212_CMCS.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// This is to add MVC services to the containers in order to support the controllers and views
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // THis is to configure the database to use SQL servrs
 
 builder.Services.AddDistributedMemoryCache();
 builder.Services.AddSession();
@@ -16,6 +21,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseSession();
 
+// This is to configure default route patterns for the MVC controllers
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
