cc15b7be75b895ee08672f2e08b8ac991d763dc8
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-16 13:24:34 +0200

    Basic ClaimTests

---
+++ ST10085639_PROG6212_CMCS.Tests/ClaimTests.cs
@@ -0,0 +1,17 @@
+﻿//Refernces:
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ST10085639_PROG6212_CMCS.Models;
+using System;
+
+namespace ST10085639_PROG6212_CMCS.Tests.Models
+{
+    [TestClass]
+    public class ClaimTests
+    {
+        [TestMethod]
+        public void CalculateTotal_ShouldReturnCorrectAmount()
+        {
+        }
+    }
+}
