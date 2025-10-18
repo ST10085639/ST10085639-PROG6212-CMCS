235d6bd759907eca49040e8e6a883923c0678703
Raeez Spence <ST10085639@vcconnect.edu.za>
2025-10-18 15:50:15 +0200

    Added comments and references to AuthenticationController

--- ST10085639_PROG6212_CMCS.Tests/AuthenticationControllerTests.cs
+++ ST10085639_PROG6212_CMCS.Tests/AuthenticationControllerTests.cs
@@ -1,4 +1,7 @@
-﻿//References:
+﻿// Microsoft Learn. 2024. Unit testing controller logic in ASP.NET Core, 17 July 2024. [Online]. Available at: https://learn.microsoft.com/en-us/aspnet/core/mvc/controllers/testing [Accessed 18 October 2025].
+// Microsoft Learn. 2022. In-memory database provider - EF Core, 23 November 2022. [Online]. Available at:https://learn.microsoft.com/en-us/ef/core/testing/in-memory [Accessed 18 October 2025].
+// Sharma, M. 2023. Mastering Unit Testing in .NET 6: Build Reliable and Maintainable Applications with xUnit and MSTest. Packt Publishing, 2023. [Online]. Available at: https://www.packtpub.com/en-us/product/mastering-unit-testing-in-net-6-9781803241766 [Accessed 18 October 2025].
+
 
 using System;
 using System.Collections.Generic;
@@ -13,6 +16,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ST10085639_PROG6212_CMCS.Tests.Controllers
 {
+// This uses MSTest to define a test class for the AuthenticationController
     [TestClass]
     public class AuthenticationControllerTests
     {
@@ -33,7 +37,7 @@ namespace ST10085639_PROG6212_CMCS.Tests.Controllers
         [TestMethod]
         public void SignUp_ValidUser_RedirectsToLogin()
         {
-            //Arrange
+            // This creates a valid user instance
             var user = new User
             {
                 FirstName = "John",
@@ -44,10 +48,10 @@ namespace ST10085639_PROG6212_CMCS.Tests.Controllers
                 Role = "Lecturer"
             };
 
-            //Action
+            // This calls the SignUp method
             var result = _controller.SignUp(user) as RedirectToActionResult;
 
-            //Assert
+            // This ensures the user(s) is redirected to the login page
             Assert.IsNotNull(result);
             Assert.AreEqual("Login", result.ActionName);
         }
@@ -55,14 +59,14 @@ namespace ST10085639_PROG6212_CMCS.Tests.Controllers
         [TestMethod]
         public void SignUp_InvalidModel_ReturnsView()
         {
-            //Arrange
+            // A error sent to the ModelState to show a input is ivalid
             _controller.ModelState.AddModelError("Email", "Required");
             var user = new User();
 
-            //Action
+            // Attempting to sign up with invalid data
             var result = _controller.SignUp(user) as ViewResult;
 
-            //Assert
+            // To make sure the view is returned and the model data is preserved
             Assert.IsNotNull(result);
             Assert.AreEqual(user, result.Model);
         }
